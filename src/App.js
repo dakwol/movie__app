ff4dfb8a5f069c27dc0db2aaed46d99b15a58e2c
Dakwol <daniilfomenko99@gmail.com>
2021-12-22 12:54:22 +0300

    react_Movie

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+function Food({name, picture}){
+  return (<div>
+    <h3> Я люблю {name}</h3>
+    <img src={picture}></img>
+  </div>)
+}
+
+const foodLike = [
+  {
+    'id': 1,
+    'name':'борщ',
+    'image': 'https://s1.eda.ru/StaticContent/Photos/121124234750/131219150003/p_O.jpg',
+    'rating': 4.8
+  },
+  {
+    'id': 2,
+    'name':'плов',
+    'image': 'https://s1.eda.ru/StaticContent/Photos/121124234750/131219150003/p_O.jpg',
+    'rating': 4.4
+  },
+  {
+    'id': 3,
+    'name':'компот',
+    'image': 'https://s1.eda.ru/StaticContent/Photos/121124234750/131219150003/p_O.jpg',
+    'rating': 4.3
+  }
+]
+
+function renderFood(dish){
+  return <Food key={dish.id} name={dish.name} picture={dish.image} />
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodLike.map(renderFood)}
     </div>
   );
 }
